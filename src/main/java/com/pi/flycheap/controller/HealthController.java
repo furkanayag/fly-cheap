1f595d44e38d13abeb3a6c189f5533b5309ec0da
furkanayag <furkan_ayag@outlook.com>
2025-01-15 15:48:18 +0300

    15.01.2025 updates

---
+++ src/main/java/com/pi/flycheap/controller/HealthController.java
@@ -0,0 +1,14 @@
+package com.solidict.hedefbackend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health-check")
+public class HealthController {
+    @GetMapping
+    public String get(){
+        return "OK";
+    }
+}
